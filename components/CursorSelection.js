5a1ac90198f428fd5c0dcad434f4d30f3bbf328a
Alan Yao <alanyao.training@gmail.com>
2023-06-02 20:42:09 -0700

    proceed

--- components/CursorSelection.js
+++ components/CursorSelection.js
@@ -1,24 +1,40 @@
 import React, { useState } from 'react';
 import styles from '@/styles/CursorSelection.module.css';
 import { useCursor } from '@/components/CursorProvider';
+import { cursorToColor } from '@/components/helpers';
 
 const CursorSelection = () => {
   const { cursor, setCursor } = useCursor();
+  const [isCursorSelected, setIsCursorSelected] = useState(false);
+  const [isButtonHovered, setIsButtonHovered] = useState(false);
+
   function handleCursorSelection(cursorFile) {
     setCursor(cursorFile);
+    setIsCursorSelected(true);
   }
 
   return (
     <div
-      className={`text-3xl text-white h-screen w-screen bg-black bg-cover bg-center flex flex-col justify-center items-center`}
+      className={`text-3xl text-white  flex flex-col h-screen w-screen bg-black bg-cover bg-center justify-center items-center`}
       style={{ cursor: `url(${cursor}), auto` }}
     >
-      <h1 className={`${styles.heading} mb-10`}>Choose your lightsaber</h1>
-      <div className="flex flex-row">
+      <h1 className={`${styles.heading} mb-8 text-center`}>Choose your lightsaber</h1>
+      <div className="flex flex-row mb-4">
         <button className={`${styles.customCursor} ${styles.blue} w-16 h-16 m-1 border-2 border-white rounded-md outline-none transition-colors duration-300 bg-blue-500 hover:border-blue-500`} onClick={() => handleCursorSelection('/blue-saber.cur')}></button>
         <button className={`${styles.customCursor} ${styles.red} w-16 h-16 m-1 border-2 border-white rounded-md outline-none transition-colors duration-300 bg-red-500 hover:border-red-500`} onClick={() => handleCursorSelection('/red-saber.cur')}></button>
         <button className={`${styles.customCursor} ${styles.green} w-16 h-16 m-1 border-2 border-white rounded-md outline-none transition-colors duration-300 bg-green-500 hover:border-green-500`} onClick={() => handleCursorSelection('/green-saber.cur')}></button>
         <button className={`${styles.customCursor} ${styles.purple} w-16 h-16 m-1 border-2 border-white rounded-md outline-none transition-colors duration-300 bg-purple-500 hover:border-purple-500`} onClick={() => handleCursorSelection('/purple-saber.cur')}></button>
+      </div>
+      <div style={{ visibility: isCursorSelected ? 'visible' : 'hidden' }}>
+        <button
+          className={`text-sm mt-8 border-2 border-white text-white px-2 py-1 rounded-md outline-none transition-all delay-100 duration-1000 ${isCursorSelected ? 'opacity-100' : 'opacity-0'}`}
+          onClick={() => console.log('Proceed')}
+          style={{ color: isButtonHovered ? cursorToColor(cursor) : 'white' }}
+          onMouseEnter={() => setIsButtonHovered(true)}
+          onMouseLeave={() => setIsButtonHovered(false)}
+        >
+          Proceed
+        </button>
       </div>
     </div>
   );
---
+++ components/helpers.js
@@ -0,0 +1,12 @@
+
+export function cursorToColor(cursorFile) {
+  if (cursorFile === '/blue-saber.cur') {
+    return '#0077FF';
+  } else if (cursorFile === '/red-saber.cur') {
+    return '#FF0000';
+  } else if (cursorFile === '/green-saber.cur') {
+    return '#01EB14';
+  } else if (cursorFile === '/purple-saber.cur') {
+    return '#D000FF';
+  }
+}
