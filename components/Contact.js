8c66af55bcaa6b19364440d696be036683810ca2
Alan Yao <alanyao.training@gmail.com>
2023-06-03 21:21:58 -0700

    contact color

--- components/Contact.js
+++ components/Contact.js
@@ -1,8 +1,13 @@
+import { useCursor } from "@/components/CursorProvider";
+import { borderColors, textColors } from '@/components/helpers';
+
 
 export default function Contact() {
+  const { cursor } = useCursor();
+
   return (
     <div className={`text-2xl`}>
-      <div className="py-4 px-8 border rounded-xl">
+      <div className={`py-4 px-8 border-2 rounded-xl ${borderColors[cursor]} hover:border-opacity-50 ${textColors[cursor]} transition duration-300`}>
         <a href="mailto:alanyao.training@gmail.com">Transmit Hologram</a> 
       </div>
     </div>
