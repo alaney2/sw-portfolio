87d6dbe16565ba72d335d222b246186c57577eef
Alan Yao <alanyao.training@gmail.com>
2023-06-04 02:25:34 -0700

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '@/styles/globals.css'
 import CursorProvider from '@/components/CursorProvider';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <CursorProvider>
       <Component {...pageProps} />
+      <Analytics />
     </CursorProvider>
   );
 };
