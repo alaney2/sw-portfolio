fbe4f5d6fda5ed0ace0a0de177830275d39af374
Alan Yao <alanyao.training@gmail.com>
2023-06-04 02:24:20 -0700

    desc

--- components/TheForceWithin.js
+++ components/TheForceWithin.js
@@ -10,11 +10,11 @@ export default function TheForceWithin({ mb }) {
     <div className={`mb-${mb} text-md leading-relaxed text-gray-400`}>
       <p className={`mb-5`}>
         Greetings, I am a software engineer based in the Outer Rim Territories of the galaxy.
-        Throughout my academic journey, I have had the opportunity to complete three internships at leading tech companies, where I honed my skills in various domains such as droid programming, hyperdrive optimization, and cloud city computing.
+        Throughout my academic odyssey, I have had the opportunity to complete three internships at leading tech companies, where I honed my skills in various domains such as droid programming, hyperdrive optimization, and cloud city computing.
       </p>
 
       <p>
-        When I&apos;m not busy coding or practicing my lightsaber skills, I love to explore my creative side through hobbies like HoloNet photography and cantina music production.
+        When I&apos;m not immersed in coding or practicing my lightsaber techniques, I enjoy exploring my creative side through hobbies like HoloNet photography and cantina music production.
         As an aspiring {side === 'Sith' ? 'Sith Lord' : 'Jedi Knight'}, I am driven by the desire to create meaningful and impactful solutions that improve lives across the galaxy.
         Don&apos;t hesitate to reach out if you&apos;d like to connect or discuss potential collaborations in our quest to restore balance to the Force.
       </p>
