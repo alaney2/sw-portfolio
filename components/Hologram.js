ce1d4cd3acd7802034f1a9521cc8d85ecebd0907
Alan Yao <alanyao.training@gmail.com>
2023-06-05 19:29:50 -0700

    jank animation

--- components/Hologram.js
+++ components/Hologram.js
@@ -2,37 +2,52 @@ import { useInView } from 'react-intersection-observer';
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 
-export default function Hologram({ mobile=false }) {
-  const { inView, ref } = useInView({ threshold: 1 });
+export default function Hologram() {
+  const { inView, ref } = useInView({ threshold: 0.1 });
   const [init, setInit] = useState(false);
+  const [visible, setVisible] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
       setInit(true)
-    }, 2000)
+    }, 100)
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setVisible(true);
+    }, 200);
   }, []);
 
   return (
     <div ref={ref}>
-      <Image
-        draggable={false}
-        style={{ zIndex: -1 }}
-        src="/holo.png"
-        alt="Hologram"
-        width={200}
-        height={200}
-        className={`profile-pic ${inView ? (!init ? 'animate' : 'show') : init ? 'hide' : 'hidden'}`}
-      />
+      {visible && (
+      <>
+        <Image
+          draggable={false}
+          style={{ zIndex: -1 }}
+          src="/holo.png"
+          alt="Hologram"
+          width={200}
+          height={200}
+          // className={`profile-pic ${inView ? (!init ? 'animate' : 'show') : init ? 'hide' : 'hidden'}`}
+          className={`profile-pic ${inView ? (!init ? (visible ? 'animate' : '') : 'show') : init ? 'hide' : 'hidden'}`}
 
-      <Image
-        draggable={false}
-        style={{ zIndex: -1 }}
-        src="/holo.png"
-        alt="Hologram"
-        width={200}
-        height={200}
-        className={`loading-profile-pic ${inView && !init ? 'show' : 'hidden'}`}
-      />
+        />
+
+        <Image
+          draggable={false}
+          style={{ zIndex: -1 }}
+          src="/holo.png"
+          alt="Hologram"
+          width={200}
+          height={200}
+          // className={`loading-profile-pic ${inView && !init ? 'show' : 'hidden'}`}
+          className={`loading-profile-pic ${inView && !init && visible ? 'show' : 'hidden'}`}
+
+        />
+      </>
+            )}
     </div>
   );
 };
